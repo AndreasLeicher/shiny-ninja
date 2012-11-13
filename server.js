20a16d9ea208fa465566735c7a89fbfd123ad6c3
Andreas Leicher <xandreasx@gmail.com>
2012-11-13 14:20:37 +0100

    added requesthandlers.js file

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var http = require("http");
 var url = require("url");
 
 
-function start(){
+function start(route){
 	function onRequest(req, res) {
 		var pathname = url.parse(req.url).pathname;
 		console.log("Request for " + pathname + " received");
+
+		//call router
+		route(pathname);
+
 		res.writeHead(200, {"Content-Type": "text/plain"});
 		res.write("Hello World 3");
 		res.end();
