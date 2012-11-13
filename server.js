e0c14f036550fb773a5121508a833f2566686528
Andreas Leicher <xandreasx@gmail.com>
2012-11-13 15:21:56 +0100

    removed post data processing, passing req object

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 //requires
-var formidable = require('formidable');
 var http = require('http');
 var url = require('url');
 
 
 function start(route, handle){
 	function onRequest(req, res) {
-		// store POST data
-		var postData = "";
 		// store request pathname
 		var pathname = url.parse(req.url).pathname;
 		console.log("Request for " + pathname + " received");
-		
-		//handle POST data extraction
-		req.setEncoding("utf8");
 
-		//add data listener
-		req.addListener("data", function(postDataChunk) {
-			postData += postDataChunk;
-			console.log("Received POST data chunk '" + postDataChunk +"'.");
-
-		});
-		req.addListener("end", function() {
-			//call router
-			route(handle, pathname, res, postData);
-		});
+		//call router with res and req objects
+		route(handle, pathname, res, req);
 	}
 
 	http.createServer(onRequest).listen(8888);
