cdc17e6dc4f4ef9c10cf5d2f61801cd39951a88b
Andreas Leicher <xandreasx@gmail.com>
2012-11-13 14:22:49 +0100

    added request handler mapping to index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+//define request handler mapping
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
 
 server.start(router.route);
